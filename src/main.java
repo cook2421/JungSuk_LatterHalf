0ca828045061237627f46004903b791420781f39
anylogic_국승현 <cooksh2421>
2021-04-12 18:28:24 +0900

    스택 큐 문제1

--- src/main.java
+++ src/main.java
@@ -1,9 +1,38 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class main{
     public static void main(String[] args) {
 
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
+        int result = 1;
 
+        int totWeight = 0;
+
+        for(int i=0; i<truck_weights.length; i++){
+            if(totWeight <= weight){
+
+            } else {
+
+            }
+        }
 
     }
 }
+
+// 트럭 덩어리 수 * (길이 + 1)
+
+
+/*
+    int bridge_length = 100;
+    int weight = 100;
+    int[] truck_weights = {10};
+    int result = 101;
+
+    int bridge_length = 100;
+    int weight = 100;
+    int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
+    int result = 110;
+*/
