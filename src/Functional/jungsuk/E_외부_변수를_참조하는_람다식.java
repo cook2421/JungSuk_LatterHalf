c41e7c5a23e8722d6a26de8b7590b0b3ca3d0b73
COOKYI\cooks <cook2421>
2021-08-10 23:57:56 +0900

    804p.까지

--- src/Functional/jungsuk/E_외부_변수를_참조하는_람다식.java
+++ src/Functional/jungsuk/E_외부_변수를_참조하는_람다식.java
@@ -16,11 +16,11 @@ class Outer {
             int val = 30;       // final int val = 30;
 //            i = 10;             // 에러. 상수의 값을 변경할 수 없음.
 
-            MyFunction3 f = () -> {
-                System.out.println("             i :" + i);
-                System.out.println("           val :" + val);
-                System.out.println("      this.val :" + ++this.val);
-                System.out.println("Outer.this.val :" + ++Outer.this.val);
+            MyFunction3 f = () -> {                                 // MyFunction3 f = (i) -> {     에러. 외부 지역변수와 이름이 중복됨.
+                System.out.println("             i :" + i);                     // 100
+                System.out.println("           val :" + val);                   // 30
+                System.out.println("      this.val :" + ++this.val);            // 21
+                System.out.println("Outer.this.val :" + ++Outer.this.val);      // 11
             };
 
             f.myMethod();
@@ -36,3 +36,13 @@ public class E_외부_변수를_참조하는_람다식 {
         inner.method(100);
     }
 }
+
+/*
+이 예제는 람다식 내에서 외부에 선언된 변수에 접근하는 방법을 보여준다.
+람다식 내에서 참조하는 지역변수는 final이 붙지 않았어도 상수로 간주된다.
+람다식 내에서 지역변수 i와 val을 참조하고 있으므로 람다식 내에서나 다른 어느 곳에서도 이 변수들의 값을 변경하는 일은 허용되지 않는다.
+
+반면에 Inner 클래스와 Outer 클래스의 인스턴스 변수인 this.val과 Outer.this.val은 상수로 간주되지 않으므로 값을 변경해도 된다.
+
+그리고 외부 지역변수와 같은 이름의 람다식 매개변수는 허용되지 않는다.
+*/
